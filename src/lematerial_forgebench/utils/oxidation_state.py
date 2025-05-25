4acc14fd53b32e985408c137a5264c2e7ad6b77d
samgleason <samuel.gleason@entalpic.ai>
2025-05-25 03:39:30 +0200

    feat: add module for oxidation state labeling and scoring for structures that Pymatgen can't determine oxidation state information for

---
+++ src/lematerial_forgebench/utils/oxidation_state.py
@@ -0,0 +1,148 @@
+import json
+from collections import defaultdict
+from itertools import combinations_with_replacement, product
+from pymatgen.core.periodic_table import Species
+
+def compositional_oxi_state_guesses(
+    comp, 
+    all_oxi_states: bool,
+    max_sites: int | None,
+    oxi_states_override: dict[str, list] | None,
+    target_charge: float,
+) -> tuple[tuple, tuple, tuple]:
+    """Utility operation for guessing oxidation states. Adapted from the _get_oxi_state_guesses function from Pymatgen.core.Composition
+
+    See `oxi_state_guesses` for full details. This operation does the
+    calculation of the most likely oxidation states
+
+    Args:
+        comp: A Pymatgen composition object. 
+        oxi_states_override (dict): dict of str->list to override an element's common oxidation states, e.g.
+            {"V": [2,3,4,5]}.
+        target_charge (float): the desired total charge on the structure. Default is 0 signifying charge balance.
+        all_oxi_states (bool): if True, all oxidation states of an element, even rare ones, are used in the search
+            for guesses. However, the full oxidation state list is *very* inclusive and can produce nonsensical
+            results. If False, the icsd_oxidation_states list is used when present, or the common_oxidation_states
+            is used when icsd_oxidation_states is not present. These oxidation states lists comprise more
+            commonly occurring oxidation states and results in more reliable guesses, albeit at the cost of
+            missing some uncommon situations. The default is False.
+        max_sites (int): if possible, will reduce Compositions to at most
+            this many sites to speed up oxidation state guesses. If the
+            composition cannot be reduced to this many sites a ValueError
+            will be raised. Set to -1 to just reduce fully. If set to a
+            number less than -1, the formula will be fully reduced but a
+            ValueError will be thrown if the number of atoms in the reduced
+            formula is greater than abs(max_sites).
+
+    Returns:
+        list[dict]: Each dict maps the element symbol to a list of
+            oxidation states for each site of that element. For example, Fe3O4 could
+            return a list of [2,2,2,3,3,3] for the oxidation states of the 6 Fe sites.
+            If the composition is not charge balanced, an empty list is returned.
+    """
+    # Reduce Composition if necessary
+    if max_sites and max_sites < 0:
+        comp = comp.reduced_composition
+
+        if max_sites < -1 and comp.num_atoms > abs(max_sites):
+            raise ValueError(f"Composition {comp} cannot accommodate max_sites setting!")
+
+    elif max_sites and comp.num_atoms > max_sites:
+        reduced_comp, reduced_factor = comp.get_reduced_composition_and_factor()
+        if reduced_factor > 1:
+            reduced_comp *= max(1, int(max_sites / reduced_comp.num_atoms))
+            comp = reduced_comp  # as close to max_sites as possible
+        if comp.num_atoms > max_sites:
+            raise ValueError(f"Composition {comp} cannot accommodate max_sites setting!")
+
+    # Load prior probabilities of oxidation states, used to rank solutions
+
+    with open("oxi_dict_probs.json", "r") as f:
+        loaded_dict = json.load(f)
+    type(comp).oxi_prob = loaded_dict
+    oxi_states_override = oxi_states_override or {}
+    # Assert Composition only has integer amounts
+    if not all(amt == int(amt) for amt in comp.values()):
+        raise ValueError("Charge balance analysis requires integer values in Composition!")
+
+    # For each element, determine all possible sum of oxidations
+    # (taking into account nsites for that particular element)
+    el_amt = comp.get_el_amt_dict()
+    elements = list(el_amt)
+    el_sums: list = []  # matrix: dim1= el_idx, dim2=possible sums
+    el_sum_scores: defaultdict = defaultdict(set)  # dict of el_idx, sum -> score
+    el_best_oxid_combo: dict = {}  # dict of el_idx, sum -> oxid combo with best score
+    for idx, el in enumerate(elements):
+        el_sum_scores[idx] = {}
+        el_best_oxid_combo[idx] = {}
+        el_sums.append([])
+        if oxi_states_override.get(el):
+            oxids: list | tuple = oxi_states_override[el]
+        elif all_oxi_states:
+            oxids = Element(el).oxidation_states
+        else:
+            oxids = Element(el).icsd_oxidation_states or Element(el).common_oxidation_states
+
+        # Get all possible combinations of oxidation states
+        # and sum each combination
+        for oxid_combo in combinations_with_replacement(oxids, int(el_amt[el])):
+            # List this sum as a possible option
+            oxid_sum = sum(oxid_combo)
+            if oxid_sum not in el_sums[idx]:
+                el_sums[idx].append(oxid_sum)
+
+            # Determine how probable is this combo?
+            scores = []
+            for o in oxid_combo: 
+                scores.append(type(comp).oxi_prob[str(Species(el, o))])
+            score = min(scores) 
+            # If it is the most probable combo for a certain sum,
+            # store the combination
+            if oxid_sum not in el_sum_scores[idx] or score > el_sum_scores[idx].get(oxid_sum, 0):
+                if max(oxid_combo) - min(oxid_combo) > 1: 
+                    pass
+                else:
+                    el_sum_scores[idx][oxid_sum] = score
+                    el_best_oxid_combo[idx][oxid_sum] = oxid_combo
+
+    # Determine which combination of oxidation states for each element
+    # is the most probable
+    all_sols = []  # will contain all solutions
+    all_oxid_combo = []  # will contain the best combination of oxidation states for each site
+    all_scores = []  # will contain a score for each solution
+    print(el_sum_scores)
+    for x in product(*el_sums):
+        # Each x is a trial of one possible oxidation sum for each element
+        if sum(x) == target_charge:  # charge balance condition
+            el_sum_sol = dict(zip(elements, x, strict=True))  # element->oxid_sum
+            # Normalize oxid_sum by amount to get avg oxid state
+            sol = {el: v / el_amt[el] for el, v in el_sum_sol.items()}
+            # Add the solution to the list of solutions
+            all_sols.append(sol)
+
+            # Determine the score for this solution
+            scores = []
+            for idx, v in enumerate(x):
+                scores.append(el_sum_scores[idx][v])
+            # the score is the minimum of the scores of each of the oxidation states in the composition - the goal is to find a charge 
+            # balanced oxidation state which limits the occurance of very uncommon oxidation states
+            all_scores.append(min(scores))
+            # Collect the combination of oxidation states for each site
+            all_oxid_combo.append(
+                {e: el_best_oxid_combo[idx][v] for idx, (e, v) in enumerate(zip(elements, x, strict=True))}
+            )
+    print(all_scores)
+    # Sort the solutions from highest to lowest score
+    if all_scores:
+        all_sols, all_oxid_combo = zip(
+            *(
+                (y, x)
+                for (z, y, x) in sorted(
+                    zip(all_scores, all_sols, all_oxid_combo, strict=True),
+                    key=lambda pair: pair[0],
+                    reverse=True,
+                )
+            ),
+            strict=True,
+        )
+    return tuple(all_sols), tuple(all_oxid_combo), tuple(sorted(all_scores, reverse = True))
